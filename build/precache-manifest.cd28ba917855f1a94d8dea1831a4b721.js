27f3bbfd0b79af10147ccd6031082be201182a1a
ad2969 <Clarenceadrian@gmail.com>
2019-08-07 15:50:41 -0700

    add build directory

---
+++ build/precache-manifest.cd28ba917855f1a94d8dea1831a4b721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "905a77ba258882bdb2c645ab74b6bec3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b0193a8205a1c8558c0",
+    "url": "/static/css/main.0c3497e0.chunk.css"
+  },
+  {
+    "revision": "5f04effcfd3d37620b23",
+    "url": "/static/js/2.03ec51f6.chunk.js"
+  },
+  {
+    "revision": "7b0193a8205a1c8558c0",
+    "url": "/static/js/main.45146bd0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
